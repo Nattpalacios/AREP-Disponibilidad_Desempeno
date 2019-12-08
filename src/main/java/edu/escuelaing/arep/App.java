0417833a1fcb830b690613e445166e21a44248f1
Natalia Palacios <nataplc24@gmail.com>
2019-12-08 03:05:02 -0500

    aws

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,29 @@
 package edu.escuelaing.arep;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/app")
+public class App {
+
+	@Autowired
+	Repositorio repositorio;
+	
+	@GetMapping("/{numero}")
+	public ResponseEntity<?> traerFibo(@PathVarible("numero") String x) throws Exception{
+	    try {
+	        return new ResponseEntity<>(fibo(Integer.parseInt(x)),HttpStatus.ACCEPTED);
+	    } catch (Exception ex) {
+	        return new ResponseEntity<>("Hubo un fallo",HttpStatus.NOT_FOUND);
+	    }
     }
+	
+	private static int fibo(int x) {
+		
+	}
 }
